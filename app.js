b47b093e14db1274d8f951bb99dde212d81daa91
varan027 <varanlucky02@gmail.com>
2025-02-15 21:06:36 +0530

    classlist

--- app.js
+++ app.js
@@ -2,38 +2,46 @@ const form = document.getElementById("myform")
 let nameval = document.getElementById("name");
 let age = document.getElementById("age");
 let msg = document.getElementById("msg");
+
+const today = new Date();
+let date = today.getDate()
+let month = today.getMonth()
+let year = today.getFullYear()
+
 form.addEventListener("submit", (event)=>{
   event.preventDefault();
 
   if(age.value<0){
-    msg.textContent = "Age cant be Negative dumbass"
+    msg.textContent = "Age cant be Negative"
+    msg.classList.add("error")
   }
 
-  else if(age.value == 0){
-    msg.textContent = "Fill in the details"
+  else if(age.value>=0 && age.value<=18){
+    msg.textContent = "You are not eligible"
+    msg.classList.add("error")
   }
 
-  else if(age.value<=19 && age.value>0){
-    msg.textContent = "you are not eligible"
-  }
+  else if(age.value>=20 && age.value<80){
 
-  else if(age.value>=20 && age.value<90){
-    if(nameval.value == "avinash"){
+    if(nameval.value == "avinash" || nameval.value == "varan" || nameval.value == "rumesh" || nameval.value == "kranthi"){
       msg.textContent = "Congratulations you are selected"
+      msg.classList.add("success")
     }
-    else if(nameval.value == "rumesh"){
-      msg.textContent = "Congratulations you are selected"
+
+    else {
+      msg.textContent = "Please visit MAHANKALI PICTURES on " + `${date+3}/${month+1}/${year}` + " at Ramoji Film City."
+      msg.classList.add("visit")
     }
-    else if(nameval.value == "varan"){
-      msg.textContent = "Congratulations you are selected"
-    }
-    
+
   }
 
-  else if(age.value>=90){
-    msg.textContent = "You are too OLD grandpa"
+  else {
+    msg.textContent = "You are too OLD"
+    msg.classList.add("error")
   }
+
 })
 function darkmode(){
 document.getElementsByTagName("body")[0].classList.toggle("bg-color")
+document.getElementsByTagName("div")[0].classList.toggle("in-color")
 }
